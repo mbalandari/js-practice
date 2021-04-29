27e5f480fa7376e3a476508c3f31e12885e5d3e6
mbalandari <m.balandari.toroghi@gmail.com>
2021-04-29 13:23:44 +0300

    first commit

---
+++ hi.js
@@ -0,0 +1,12 @@
+var coder = "Maz";
+var twittName = "codingMaz";
+var age = 39;
+
+function whoAmI(myName, myNickname, myAge) {
+    console.log(`
+        Hi, my name is ${myName}. My nickname is ${myNickname}.
+        I am ${myAge} years old.
+    `);
+}
+
+whoAmI(coder, twittName, age);
